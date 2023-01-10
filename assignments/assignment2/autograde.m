655ccd209ab8d80828150a3c1ad3f3e0b388643a
Karn Watcharasupat <karn001@e.ntu.edu.sg>
2023-01-10 14:52:38 -0500

    Repo initialization

---
+++ assignments/assignment2/autograde.m
@@ -0,0 +1,12 @@
+% AUTOGRADER: STUDENTS' VERSION
+
+function autograde(~, ~, studentId, ~, ~)
+
+    if nargin < 4
+        disp('')
+    end
+
+    if studentId == "gburdell3"
+        error("You forgot to set your student ID!");
+    end
+end
